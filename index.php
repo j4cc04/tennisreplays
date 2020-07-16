48b4367a7c658969f8566aa5480f22e5d5657036
Goofy <j4cc04@gmail.com>
2020-07-16 14:48:44 +0100

    make it betters

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
   <title>PHP Test</title>
  </head>
  <body>
+ <?php
+
+    $str = file_get_contents('list.json');
+    $json = json_decode($str, true);
+    foreach($json as $match){
+        echo '<span>' . $match["p1"] . '</span><span>' . $match["p2"] . '</span><br />';
+    }
+?> 
  <?php echo '<p>Hello Worlda</p>'; ?> 
  </body>
 </html>
+
